31aa655c60afc97967b75c983c6500dad74c2534
bassam-abbasi <bassam_abbssi@hotmail.com>
2022-08-25 10:53:35 +0000

    diagram-1

--- app/simple-net-diagram.py
+++ app/simple-net-diagram.py
@@ -1,15 +1,8 @@
+# diagram.py
 from diagrams import Diagram
 from diagrams.aws.compute import EC2
-import sys
-if getattr(sys, 'frozen', False):
-    import os
-    path = os.environ.get('PATH')
-    print("path={}".format(path))
-    if path:
-         path = path + os.pathsep + sys._MEIPASS
-    else:
-         path = sys._MEIPASS
-    os.environ['PATH'] = path
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
 
-with Diagram("Network Diagram"):
-    EC2("web")
+with Diagram("Web Service", show=False):
+    ELB("lb") >> EC2("web") >> RDS("userdb")
