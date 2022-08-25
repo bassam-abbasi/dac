ebf534e70056f9ee53202ff490ace3cc66322090
bassam-abbasi <bassam_abbssi@hotmail.com>
2022-08-25 15:03:46 +0000

    1

--- app/diagram2.py
+++ app/diagram2.py
@@ -1,8 +1,13 @@
-from diagrams import Diagram
+from diagrams import Diagram,Cluster
 from diagrams.generic.network import Router
 from diagrams.generic.network import Switch
 from diagrams.generic.network import Firewall
 
 with Diagram("Edge topo", show=False, direction="TB"):
-    [Router("RTEIDW001"),
-                  Switch("L2Switch")] >> Firewall("EdgeFW")
+    with Cluster("Data Center"):
+        Router("BL1")
+        Router("BL2")
+
+    with Cluster("DC Edge"):
+        Router("DCI1")
+        Router("DCI2")
